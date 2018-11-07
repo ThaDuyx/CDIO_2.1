985127fe001d27ab29a06f8fb97095df60f5c608
Duyx <sandersen98@live.dk>
2018-11-07 14:25:01 +0100

    Tilføjelse af en 4 sidet terning

--- src/Dice.java
+++ src/Dice.java
@@ -5,6 +5,8 @@ public class Dice {
     //Sætter maximum mængde af 'int øjne' til 6
     private final int MAX = 6;
 
+    private final int MAX2 = 4;
+
     //Laver terning
     public Dice() {
         face = 1;
@@ -13,6 +15,10 @@ public class Dice {
     //Beregner matematikken bag terningen ved at gange et tilfældigt tal 1-6 med terningen med 1 øje
     public void roll() {
         face = (int) (Math.random() * MAX) + 1;
+    }
+
+    public void roll2() {
+        face = (int) (Math.random() * MAX2) + 1;
     }
 
     //Henter antallet af øjne
