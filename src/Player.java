4eb6d818909bc10e4260c62dd8277ed610c2aa00
Duyx <sandersen98@live.dk>
2018-11-07 13:08:32 +0100

    Gamle projekt - commit

---
+++ src/Player.java
@@ -0,0 +1,35 @@
+public class Player {
+
+    //Opretter String
+    private String name;
+
+    //Opretter int
+    private int point;
+
+    public int getlastPoint;
+
+    //Tilføjer spiller navnet
+    public Player(String name) {
+        this.name = name;
+    }
+
+    //Henter Strengen "navn"
+    public String getName() {
+        return this.name;
+    }
+
+    //Henter Strengen "point"
+    public int getPoint() {
+        return this.point;
+    }
+
+    //Plusser det nye antal point med det totale for den enkelte spiller
+    public void addPoint(int totalpoint) {
+        this.point = this.point + totalpoint;
+    }
+
+    public int resetPoint() {
+        this.point = 0;
+        return this.point;
+    }
+}
