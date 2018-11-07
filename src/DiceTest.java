e9198b9f83c87922d4f116c466f612f566222e99
Duyx <sandersen98@live.dk>
2018-11-07 22:34:44 +0100

    Ny DiceTest på den 4-sidet terning

--- src/DiceTest.java
+++ src/DiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 public class DiceTest {
 
+    //Tester den 6-sidet terning
     @Test
     public void roll() {
         Dice dice1 = new Dice();
@@ -23,6 +24,27 @@ public class DiceTest {
 
         for (int face = 2; face < array.length; ++face) {
             System.out.printf("%2d%13d\n", face, array[face]);
+        }
+    }
+
+    //Tester den 4-sidet terning
+    @Test
+    public void roll2(){
+        Dice dice3 = new Dice();
+        Dice dice4 = new Dice();
+        int[] array = new int[9];
+
+        for (int i = 0; i <= 20000; ++i) {
+            dice3.roll2();
+            dice4.roll2();
+            int number2 = dice3.getFace() + dice4.getFace();
+
+            Assert.assertTrue(number2 > 1 && number2 < 9);
+            ++array[number2];
+            System.out.println(number2);
+        }
+        for (int face = 2; face < array.length; ++face) {
+            System.out.printf("%2d%9d\n", face, array[face]);
         }
     }
 }
